1335311fae05cc3a3c523be6b97ca73a1bb7ec5a
barteloniu <barteloniu@gmail.com>
2016-06-11 11:27:11 +0200

    ulepszono app.js

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var express = require("express");
 var app = express();
 var server = require("http").Server(app);
+var port = process.env.PORT || 5000;
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/client/index.html")
 });
 app.use("/client", express.static(__dirname + "/client"));
-server.listen(process.env.PORT || 5000);
-console.log("Server started!");
+server.listen(port);
+console.log("Server started on port " + port);
