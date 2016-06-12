bc5ef1a9987b2160aa2fea0236c83c3136daa60e
barteloniu <barteloniu@gmail.com>
2016-06-12 13:07:04 +0200

    ulepszono client

--- client/skrypt.js
+++ client/skrypt.js
@@ -1,28 +1,29 @@
 var ctx = document.getElementById("canvas").getContext("2d");
 
 var keys = [];
+function start() {
+  var socket = io();
+  socket.on("ileOnline", function (data) {
+    document.getElementById("tekst").innerHTML = "Online: " + data + "!";
+  });
 
-var socket = io();
-socket.on("ileOnline", function (data) {
-  document.getElementById("tekst").innerHTML = "Online: " + data + "!";
-});
+  socket.on("players", function (data) {
+    ctx.clearRect(0, 0, 500, 500);
+    for(var i = 0;i < data.length; i++){
+      ctx.fillStyle = data[i].color;
+      ctx.beginPath();
+      ctx.arc(data[i].x, data[i].y, 20, 0, 2 * Math.PI);
+      ctx.fill();
+    }
+  });
 
-socket.on("players", function (data) {
-  ctx.clearRect(0, 0, 500, 500);
-  for(var i = 0;i < data.length; i++){
-    ctx.fillStyle = data[i].color;
-    ctx.beginPath();
-    ctx.arc(data[i].x, data[i].y, 20, 0, 2 * Math.PI);
-    ctx.fill();
-  }
-});
+  document.onkeydown = function (event) {
+    keys[event.keyCode] = true;
+    socket.emit("keypress", {keys: keys});
+  };
 
-document.onkeydown = function (event) {
-  keys[event.keyCode] = true;
-  socket.emit("keypress", {keys: keys});
-};
-
-document.onkeyup = function (event) {
-  delete keys[event.keyCode];
-  socket.emit("keypress", {keys: keys});
-};
+  document.onkeyup = function (event) {
+    delete keys[event.keyCode];
+    socket.emit("keypress", {keys: keys});
+  };
+}
