d3dac05901861f1ea073dac8e6610cfe32c38dad
barteloniu <barteloniu@gmail.com>
2016-06-14 21:20:48 +0200

    ulepszono poruszanie sie

--- app.js
+++ app.js
@@ -26,15 +26,14 @@ var Player = function (id) {
     nick:""
   };
   self.updatePos = function () {
+
+    self.speedX*= 0.9;
+    self.speedY*= 0.9;
+
     if(self.czyD)self.speedX+= 0.2;
     if(self.czyA)self.speedX-= 0.2;
     if(self.czyS)self.speedY+= 0.2;
     if(self.czyW)self.speedY-= 0.2;
-
-    if(!self.czyD && self.speedX >= 0) self.speedX-= 0.2;
-    if(!self.czyA && self.speedX <= 0) self.speedX+= 0.2;
-    if(!self.czyS && self.speedY >= 0) self.speedY-= 0.2;
-    if(!self.czyW && self.speedY <= 0) self.speedY+= 0.2;
 
     if(self.speedX < -5) self.speedX = -5;
     if(self.speedX > 5) self.speedX = 5;
