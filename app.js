e1583086b01ee0cceda79e9b2391853f9fb095c4
barteloniu <barteloniu@gmail.com>
2016-06-14 20:47:31 +0200

    ulepszono porusznie sie

--- app.js
+++ app.js
@@ -31,10 +31,10 @@ var Player = function (id) {
     if(self.czyS)self.speedY+= 0.1;
     if(self.czyW)self.speedY-= 0.1;
 
-    if(!self.czyD && self.x > 0) self.speedX--;
-    if(!self.czyA && self.x < 0) self.speedX++;
-    if(!self.czyS && self.y > 0) self.speedY--;
-    if(!self.czyW && self.y < 0) self.speedY++;
+    if(!self.czyD && self.speedX > 0) self.speedX-= 0.1;
+    if(!self.czyA && self.speedX < 0) self.speedX+= 0.1;
+    if(!self.czyS && self.speedY > 0) self.speedY-= 0.1;
+    if(!self.czyW && self.speedY < 0) self.speedY+= 0.1;
 
     if(self.speedX < -5) self.speedX = -5;
     if(self.speedX > 5) self.speedX = 5;
